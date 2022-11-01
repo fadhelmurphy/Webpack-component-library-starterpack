4118ff72f06b9ca2e765b72abad12922c49165a0
Padelpop Eskrim <fadhel@femaledaily.com>
2022-11-02 00:14:01 +0700

    initiate

--- build-utils/webpack.config.js
+++ build-utils/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = (env, {mode}) => ({
 	output: {
 		filename: 'index.js',
 		path: path.resolve(__dirname, 'dist'),
-		libraryTarget: 'commonjs'
+		libraryTarget: 'umd'
 	},
 	plugins: [new CleanWebpackPlugin()],
 	module: {
